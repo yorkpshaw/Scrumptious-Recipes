70e2b13214971a3bdec745796f2f8558ed62972e
York <york.p.shaw@gmail.com>
2022-08-28 22:31:13 -0500

    Added the name and author models

---
+++ recipes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " by " + self.author
