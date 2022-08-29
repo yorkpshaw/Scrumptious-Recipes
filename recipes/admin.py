0b06e5b87edb34184913c6f2c7f03b0e6d4257b6
York <york.p.shaw@gmail.com>
2022-08-28 23:27:24 -0500

    Added the Ingredient Model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Recipe
+from .models import Measure
+from .models import FoodItem
+from .models import Ingredient
 
 admin.site.register(Recipe)
+
+admin.site.register(Measure)
+
+admin.site.register(FoodItem)
+
+admin.site.register(Ingredient)
--- recipes/models.py
+++ recipes/models.py
@@ -11,3 +11,44 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name + " by " + self.author
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name + f"({self.abbreviation})"
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name="ingredients",
+        on_delete=models.CASCADE,
+    )
+    measure = models.ForeignKey(
+        "Measure",
+        on_delete=models.PROTECT,
+    )
+    food = models.ForeignKey(
+        "FoodItem",
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return self.recipe
+
+    def __str__(self):
+        return self.measure
+
+    def __str__(self):
+        return self.food
